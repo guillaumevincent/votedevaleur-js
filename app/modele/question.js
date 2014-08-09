ac9010af713cc755ade575f2614232666334cf4a
Guillaume Vincent <gvincent@oslab.fr>
2014-08-09 20:57:21 +0200

    permettre à une question de donner une réponse

--- app/modele/question.js
+++ app/modele/question.js
@@ -2,6 +2,44 @@ function Question(question) {
     this.intitulé = question.intitulé;
     this.choix = question.choix;
     this.opinions = [];
+
+    this.ajouterUneOpinion = function (opinion) {
+        this.opinions.push(opinion);
+    };
+
+    this.compterOpinions = function (opinions) {
+        var résultats = {};
+        opinions.forEach(function (opinion) {
+            opinion.notes.forEach(function (note) {
+                if (note.choix in résultats) {
+                    résultats[note.choix] += note.valeur;
+                } else {
+                    résultats[note.choix] = note.valeur;
+                }
+            });
+        });
+        return résultats
+    };
+
+    this.récupérerLaListeDesMeilleursChoix = function (plusieursChoix) {
+        var meilleursChoix = [];
+        var valeurMaximum;
+        Object.keys(plusieursChoix).forEach(function (choix) {
+            var valeurDuChoix = plusieursChoix[choix];
+            if(typeof valeurMaximum === 'undefined' || valeurDuChoix > valeurMaximum){
+                meilleursChoix = [choix];
+                valeurMaximum = valeurDuChoix;
+            }else if(valeurDuChoix == valeurMaximum){
+                meilleursChoix.push(choix)
+            }
+        });
+        return meilleursChoix
+    };
+
+    this.obtenirUneRéponse = function () {
+        var opinionsComptées = this.compterOpinions(this.opinions);
+        return this.récupérerLaListeDesMeilleursChoix(opinionsComptées)
+    };
 }
 
 module.exports = Question;
--- tests/question.js
+++ tests/question.js
@@ -1,13 +1,80 @@
 var Question = require('../app/modele/Question'),
-    assert = require("assert");
+    assert = require('assert');
 
-describe('une question', function(){
-    it('doit avoir un intitulé, des choix et des opinions', function(){
-        var intitulé = 'Simple Question';
-        var choix = ['choix 1', 'choix 2'];
-        var question = new Question({intitulé: intitulé, choix: choix});
+describe('une question', function () {
+    var intitulé;
+    var choix;
+    var question;
+    var opinion;
+
+    beforeEach(function () {
+        intitulé = 'Simple Question';
+        choix = ['choix 1', 'choix 2'];
+        question = new Question({intitulé: intitulé, choix: choix});
+        opinion = {
+            electeur: "Prénom Nom",
+            notes: [
+                {choix: 'choix 1', valeur: 2},
+                {choix: 'choix 2', valeur: 0}
+            ]
+        };
+    });
+
+    it('doit avoir un intitulé, des choix et des opinions', function () {
         assert.equal(question.intitulé, intitulé);
         assert.deepEqual(question.choix, choix);
         assert.deepEqual(question.opinions, []);
     });
+
+    it("peut recevoir une opinion d'un electeur", function () {
+        question.ajouterUneOpinion(opinion);
+        assert.deepEqual(question.opinions, [opinion]);
+    });
+
+    it("a une réponse s'il y a au moins une opinion", function () {
+        question.ajouterUneOpinion(opinion);
+        assert.deepEqual(question.obtenirUneRéponse(), ['choix 1']);
+    });
+
+    it('doit compter les notes de chaque opinion', function () {
+        var opinions = [
+            {
+                electeur: 'Prénom Nom',
+                notes: [
+                    {choix: 'Choix 1', valeur: 1},
+                    {choix: 'Choix 2', valeur: 2}
+                ]
+            },
+            {
+                electeur: 'Prénom Nom',
+                notes: [
+                    {choix: 'Choix 1', valeur: -1},
+                    {choix: 'Choix 2', valeur: 2}
+                ]
+            }
+        ];
+        var opinionsComptéesAttendues = {"Choix 1": 0, "Choix 2": 4};
+        assert.deepEqual(question.compterOpinions(opinions), opinionsComptéesAttendues);
+    });
+
+    it("peut retourner le maximum des résultats", function () {
+        // todo déplacer la fonction récupérerLaListeDesMeilleursChoix dans une boite à outil JS
+        var résultats = {"Choix 1": 0, "Choix 2": 4};
+        assert.deepEqual(question.récupérerLaListeDesMeilleursChoix(résultats), ['Choix 2']);
+        var autreRésultats = {"Choix 1": -1, "Choix 2": -1};
+        assert.deepEqual(question.récupérerLaListeDesMeilleursChoix(autreRésultats), ['Choix 1', 'Choix 2']);
+    });
+
+    it("a deux réponses à égalité s'il les choix ont le même score", function () {
+        var autreOpinion = {
+            electeur: "Prénom Nom",
+            notes: [
+                {choix: 'Choix 1', valeur: 2},
+                {choix: 'Choix 2', valeur: 2}
+            ]
+        };
+        question.ajouterUneOpinion(autreOpinion);
+        assert.deepEqual(question.obtenirUneRéponse(), ['Choix 1', 'Choix 2']);
+    });
+
 });
