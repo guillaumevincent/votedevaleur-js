39416ee3c8e3be385806c55d9e3d9d6e08b2e761
Guillaume Vincent <gvincent@oslab.fr>
2014-12-14 23:15:51 +0100

    correction d'une erreur dans les routes qui masquait les requetes API

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,14 @@
 var logger = require('logger'),
-    voteControleur = require('./controleur/voteControleur');
+    voteControleur = require('./controleur/voteControleur'),
+    path = require('path');
 
 module.exports = function (app) {
-    app.use(function (req, res) {
-        logger.log('debug', 'GET %s pour %s (User Agent: %s)', req.path, req.connection.remoteAddress, req.headers['user-agent']);
-        res.sendFile('/public/index.html');
-    });
     app.post('/votes', voteControleur.créerUnVote);
     app.get('/votes/:id', voteControleur.récupérerUnVote);
     app.post('/votes/:id/opinions', voteControleur.créerOpinion);
     app.get('/:id', voteControleur.raccourciDUnVote);
+    app.get('*', function (req, res) {
+        logger.log('info', 'GET %s pour %s (User Agent: %s)', req.path, req.connection.remoteAddress, req.headers['user-agent']);
+        res.sendFile('index.html', {root: path.join(__dirname, '../public')});
+    });
 };
